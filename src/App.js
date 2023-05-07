71d3beb733bc35d20d837a9db0fe518274548eda
Varus <varushsu@gmail.com>
2023-05-07 20:52:51 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
+import tsIcon from  './img/KFC.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        你好，LJY
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a href={"http://www.kfc.com.cn/kfccda/index.aspx"}>
+          {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+        　<img src={tsIcon} alt={"Image Not Found"}/></a>
       </header>
     </div>
   );
